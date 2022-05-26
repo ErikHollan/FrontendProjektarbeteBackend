27e859815a423ed4b3fccc98e1e1b8cf7bcf2c6e
Miguel <miguel.h.ogren@gmail.com>
2022-05-26 15:37:14 +0200

    Comment add

--- src/main/java/com/example/frontendprojektarbetebackend/FrontendProjektarbeteBackendApplication.java
+++ src/main/java/com/example/frontendprojektarbetebackend/FrontendProjektarbeteBackendApplication.java
@@ -3,6 +3,7 @@ package com.example.frontendprojektarbetebackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//This is comment
 @SpringBootApplication
 public class FrontendProjektarbeteBackendApplication {
 
